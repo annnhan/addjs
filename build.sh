37d80d9e32c4cbe80c89b530182af11e17a2f623
an.han <an.han@qunar.com>
2015-02-24 22:44:48 +0800

    bugfix

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-cd ~/project/addjs/src
-zip -r addjs.zip *
-mv addjs.zip ../
-cd ~/project/hanan198501.github.io/assets/download/
-cp ~/project/addjs/src.crx ~/project/hanan198501.github.io/assets/download/addjs.crx
-zip addjs.zip addjs.crx
-git add ./
-git git commit -am "add download"
+cd ~/project/addjs/src &&\
+zip -r addjs.zip * **\
+mv addjs.zip ../ &&\
+cd ~/project/hanan198501.github.io/assets/download/ &&\
+cp ~/project/addjs/src.crx ~/project/hanan198501.github.io/assets/download/addjs.crx &&\
+zip addjs.zip addjs.crx &&\
+git add ./ &&\
+git git commit -am "add download" &&\
 git push
