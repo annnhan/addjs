18c440e60b44d85873010bf7152783cfc6280e11
an.han <an.han@qunar.com>
2015-02-24 15:31:05 +0800

    add download

---
+++ build.sh
@@ -0,0 +1,6 @@
+cd ~/project/hanan198501.github.io/assets/download/
+cp ~/project/addjs/src.crx ~/project/hanan198501.github.io/assets/download/addjs.crx
+zip addjs.zip addjs.crx
+git add ./
+git git commit -am "add download"
+git push
